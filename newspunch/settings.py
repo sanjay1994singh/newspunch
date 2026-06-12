47ed9f5d6b84686912ed46ec777afc86f206a500
sanjay1994singh <sanjaypython22@gmail.com>
2026-06-12 13:24:59 +0530

    first

--- newspunch/settings.py
+++ newspunch/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-(!c=%3+=^(hgtuw@#jom_9^0j-%9@5vkk2h++^e^-grd_g=)iz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
